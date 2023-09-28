748d1e0a4196e2876cb05d9834808ea896754409
Ian <gichaneian@gmail.com>
2023-09-28 14:37:05 +0300

    Event listener hook

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const input =document.getElementById('input')
+
+function addingEventListener(click) {
+    input.addEventListener(click,function(){alert('I was clicked!');})
+    
 }
+addingEventListener('click') 
